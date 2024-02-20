9fd7653e394a6cf8d07996153560290a1e41e593
Juan David Alvis <juanalvis6@gmail.com>
2024-02-20 11:40:42 -0500

    modify runner

--- src/donbot/behave_runner.py
+++ src/donbot/behave_runner.py
@@ -3,30 +3,20 @@ import subprocess
 
 class BehaveRunner:
     @staticmethod
-    def run_tests(steps_path="features/steps", tags=None):
+    def run_tests(tags=None):
         """
         Run Behave tests with configurable options.
 
-        :param steps_path: Path of the step definition files.
-                           Default: "features/steps".
         :param tags: Behave tags to filter tests.
         """
         command = ["behave"]
 
         # Configuration of step and feature paths
-        command.extend(["--steps", steps_path])
-        command.extend(["--format", "allure_behave.formatter:AllureFormatter -o allure-results"])
+        command.extend(["-f", "allure_behave.formatter:AllureFormatter"])
+        command.extend(["-o", "allure-results"])
 
         # Tags configuration
         if tags:
             command.extend(["--tags", tags])
 
         subprocess.call(command)
-
-    @staticmethod
-    def generate_allure_report():
-        """
-        Generate Allure report from the results and open it.
-        """
-        subprocess.call(["allure", "generate", "allure-results"])
-        subprocess.call(["allure", "open"])
