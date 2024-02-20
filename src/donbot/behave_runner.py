565cc9af8b118487e496bfd58f588954ac22a6bb
Juan David Alvis <juanalvis6@gmail.com>
2024-02-20 10:28:41 -0500

    delete features path

--- src/donbot/behave_runner.py
+++ src/donbot/behave_runner.py
@@ -3,14 +3,12 @@ import subprocess
 
 class BehaveRunner:
     @staticmethod
-    def run_tests(steps_path="features/steps", features_path="features", tags=None):
+    def run_tests(steps_path="features/steps", tags=None):
         """
         Run Behave tests with configurable options.
 
         :param steps_path: Path of the step definition files.
                            Default: "features/steps".
-        :param features_path: Path to the .feature files.
-                              Default: "features".
         :param tags: Behave tags to filter tests.
         """
         command = ["behave"]
@@ -18,8 +16,6 @@ class BehaveRunner:
         # Configuration of step and feature paths
         command.extend(["--steps", steps_path])
         command.extend(["--format", "allure_behave.formatter:AllureFormatter -o allure-results"])
-        if features_path:
-            command.extend([features_path])
 
         # Tags configuration
         if tags:
